f33a8d77d770e6557b61a724b8ec86d49cfaaba6
CBLITTMAN-OAREDA <119352183+CBLITTMAN-OAREDA@users.noreply.github.com>
2023-01-03 15:55:52 -0500

    Make syntax generic

--- SQL/change_password.sql
+++ SQL/change_password.sql
@@ -1,4 +1,4 @@
-ALTER USER cblbh IDENTIFIED BY "BethanyJ0707!" REPLACE "HelenaS0609!";
+ALTER USER cblbh IDENTIFIED BY "new" REPLACE "current";
 
---ALTER USER cblbh IDENTIFIED BY "NewPassword" REPLACE "OldPassword";
+--ALTER USER username IDENTIFIED BY "NewPassword" REPLACE "OldPassword";
 
