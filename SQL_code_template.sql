be40f7f1e1f7b6abd99d69f2b1a00d3da7e6df07
CBLITTMAN-OAREDA <119352183+CBLITTMAN-OAREDA@users.noreply.github.com>
2023-05-17 09:47:47 -0400

    Adding SQL code templates

---
+++ SQL_code_template.sql
@@ -0,0 +1,26 @@
+------------------------------------------------------------------------------------------
+-- FILE NAME: [path]\filename.sql
+
+-- DATE CREATED: 
+-- [YYYY-MM-DD]
+-- e.g., 2023-04-26
+
+-- WRITTEN BY: 
+-- [First and Last Name of original code author]
+-- e.g., Colin Chellman
+--
+-- PURPOSE AND BACKGROUND: 
+-- [Purpose]
+-- e.g., Identify stop outs to report their academic profile
+
+-- DATA TIME FRAME [
+-- [what years or semesters were queried (original)?]
+-- e.g., Five year trends, fall 2018 through fall 2022
+
+-- REVISIONS:
+-- e.g., 2021-01-01 – Change data time frame to include additional fall terms (Cheryl Littman)
+-- e.g., 2022-11-15 – Added additional code to identify students enrolled in online courses (Mark Casazza)
+------------------------------------------------------------------------------------------ 
+
+--Hint: save the code above as a snippet and drag it out to your blank .sql worksheet when you
+--begin a new project
